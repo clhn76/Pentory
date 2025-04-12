2492605f5bd2a5dc59a75abf0e84e40dc668855b
Taewon Ahn <withdandy09@naver.com>
2025-04-12 09:17:30 +0900

    ✨ 대시보드 레이아웃 및 사이드바 개선: 스페이스 정보를 사전 로드하도록 수정하고, 내 스페이스 목록을 새로운 컴포넌트로 변경하여 비동기 로딩 지원 추가

--- src/app/(dashboard)/_components/dashboard-sidebar.tsx
+++ src/app/(dashboard)/_components/dashboard-sidebar.tsx
@@ -1,35 +1,17 @@
 import { Logo } from "@/components/common/logo";
 import {
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger,
-} from "@/components/ui/collapsible";
-import {
   Sidebar,
   SidebarContent,
   SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-  SidebarMenuSub,
-  SidebarMenuSubButton,
-  SidebarMenuSubItem,
 } from "@/components/ui/sidebar";
 import { DASHBOARD_MENU_ITEMS } from "@/configs/dashboard.config";
-import { ChevronRight, FolderIcon } from "lucide-react";
 import Link from "next/link";
+import { Suspense } from "react";
+import { SidebarMySpaces, SidebarMySpacesSkeleton } from "./sidebar-my-spaces";
 import { UserPlanInfo } from "./user-plan-info";
-
-const MY_SPACES = [
-  {
-    id: 1,
-    name: "스페이스1",
-  },
-  {
-    id: 2,
-    name: "스페이스2",
-  },
-];
 
 export const DashboardSidebar = () => {
   return (
@@ -57,32 +39,9 @@ export const DashboardSidebar = () => {
           </SidebarMenu>
 
           {/* 내 스페이스 목록 */}
-          <SidebarMenu>
-            <Collapsible asChild defaultOpen className="group/collapsible">
-              <SidebarMenuItem>
-                <CollapsibleTrigger asChild>
-                  <SidebarMenuButton tooltip="내 스페이스">
-                    <FolderIcon />
-                    <span>내 스페이스</span>
-                    <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
-                  </SidebarMenuButton>
-                </CollapsibleTrigger>
-                <CollapsibleContent>
-                  <SidebarMenuSub>
-                    {MY_SPACES.map((item) => (
-                      <SidebarMenuSubItem key={item.id}>
-                        <SidebarMenuSubButton asChild>
-                          <Link href={`/dashboard/spaces/${item.id}`}>
-                            <span className="truncate">{item.name}</span>
-                          </Link>
-                        </SidebarMenuSubButton>
-                      </SidebarMenuSubItem>
-                    ))}
-                  </SidebarMenuSub>
-                </CollapsibleContent>
-              </SidebarMenuItem>
-            </Collapsible>
-          </SidebarMenu>
+          <Suspense fallback={<SidebarMySpacesSkeleton />}>
+            <SidebarMySpaces />
+          </Suspense>
         </SidebarContent>
       </Sidebar>
     </div>
